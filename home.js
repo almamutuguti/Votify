a9b072aefce1959cc4090470a84c67c03666282f
almamutuguti <alma.wanjiru8j@gmail.com>
2025-07-07 16:33:52 +0300

    add navbar and hero section

---
+++ home.js
@@ -0,0 +1,19 @@
+const navbar = document.querySelector('.navbar');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+window.addEventListener('scroll', () => {
+    if(this.scrollY >= 100) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+})
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toogle('active')
+    hamburger.classList.toggle('active')
+})
+
+
+
+
